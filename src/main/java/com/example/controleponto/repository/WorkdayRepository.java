fb7e48ed40b4a5d41db61974bde83702b93ddfa4
Arthur Araújo <arthur4raujo@gmail.com>
2021-02-08 06:04:15 -0300

    find workday by year and month method

--- src/main/java/com/example/controleponto/repository/WorkdayRepository.java
+++ src/main/java/com/example/controleponto/repository/WorkdayRepository.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 import static com.example.controleponto.repository.query.WorkdayQuery.*;
@@ -41,9 +42,16 @@ public class WorkdayRepository {
                 break;
         }
 
-        String query = SET_TIME_REGISTER.getQuery().replace(":moment", type.toString().toLowerCase());
+        String query = UPDATE_TIME_REGISTER.getQuery().replace(":moment", type.toString().toLowerCase());
         template.update(query,
                 Map.of("timestmp", dateTime.toString(),
+                        "id", workday.getId()));
+    }
+
+    public void updateWorkedSeconds(Workday workday) {
+        String query = UPDATE_WORKED_SECONDS.getQuery();
+        template.update(query,
+                Map.of("secondsWorked", workday.getSecondsWorked(),
                         "id", workday.getId()));
     }
 
@@ -55,11 +63,8 @@ public class WorkdayRepository {
         }
     }
 
-    public void updateWorkedSeconds(Workday workday) {
-        String query = UPDATE_WORKED_SECONDS.getQuery();
-        template.update(query,
-                Map.of("secondsWorked", workday.getSecondsWorked(),
-                        "id", workday.getId()));
-
+    public List<Workday> findByYearAndMonth(String year, String month) {
+        return template.query(FIND_BY_YEAR_AND_MONTH.getQuery(), Map.of("y", year,
+                "m", month), mapper);
     }
 }
--- src/main/java/com/example/controleponto/repository/query/WorkdayQuery.java
+++ src/main/java/com/example/controleponto/repository/query/WorkdayQuery.java
@@ -7,9 +7,10 @@ import lombok.Getter;
 public enum WorkdayQuery {
 
     INSERT("workday/insert-workday.sql"),
+    UPDATE_WORKED_SECONDS("workday/update-workday-worked-seconds.sql"),
+    UPDATE_TIME_REGISTER("workday/set-workday-time-register.sql"),
     FIND_BY_DATE("workday/select-workday-by-date.sql"),
-    SET_TIME_REGISTER("workday/set-workday-time-register.sql"),
-    UPDATE_WORKED_SECONDS("workday/update-workday-worked-seconds.sql");
+    FIND_BY_YEAR_AND_MONTH("workday/select-workday-by-month-and-year.sql");
 
     private final String query;
 
